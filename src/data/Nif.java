01e52b2940782c31ebf7f68324a96235ef66c4fd
Alex <alexstoia.as@gmail.com>
2022-12-17 22:56:14 +0100

    Functions to ensure NIF is valid

--- src/data/Nif.java
+++ src/data/Nif.java
@@ -1,9 +1,12 @@
 package data;
 
+
 final public class Nif {
     // The tax identification number in the Spanish state.
     private final String nif;
-    public Nif (String code) { this.nif = code; }
+    public Nif (String code) {
+        checkValidNIF(code);
+        this.nif = code; }
     public String getNif () { return nif; }
     @Override
     public boolean equals (Object o) {
@@ -17,5 +20,23 @@ final public class Nif {
     @Override
     public String toString () {
         return "Nif{" + "nif ciudadano='" + nif + '\'' + '}';
+    }
+    private void checkValidNIF (String code) {
+        if (code == null) throw new NullPointerException("NIF cannot be null");
+        if (code.length() != 9) throw new IllegalArgumentException("NIF must be 9 characters long");
+        if (!correctNIF(code)) throw new IllegalArgumentException("Incorrect NIF format, must be 8 digits and 1 capital letter");
+    }
+    private boolean correctNIF (String code) { //Check if the NIF is in the correct format, not checking NIE
+        char[] nif = code.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < 8; i++) {
+            if (!Character.isDigit(nif[i])) return false;
+            sum += Character.getNumericValue(nif[i]);
+        }
+        int residue = sum % 23;
+        char[] validLetters = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        return Character.isUpperCase(nif[8]) && nif[8] == validLetters[residue];
+
+
     }
 }
