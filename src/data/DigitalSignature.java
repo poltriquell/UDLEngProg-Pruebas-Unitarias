0f9bc7bd512b62e480e8888c056184b355ad1667
Alex <alexstoia.as@gmail.com>
2022-12-18 20:17:19 +0100

    DigitalSignature implemented

--- src/data/DigitalSignature.java
+++ src/data/DigitalSignature.java
@@ -1,5 +1,39 @@
 package data;
 
 final public class DigitalSignature {
+    private final byte[] signature;
+
+    public DigitalSignature(byte[] signature) {
+        checkSignature(signature);
+        this.signature = signature;
+    }
+
+    private void checkSignature(byte[] signature) {
+        if (signature == null) throw new NullPointerException("Signature cannot be null");
+    }
+
+    public byte[] getSignature() {
+        return signature;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DigitalSignature that = (DigitalSignature) o;
+
+        return signature.equals(that.signature);
+    }
+
+    @Override
+    public int hashCode() {
+        return signature.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "DigitalSignature{" + "signature=" + signature + '}';
+    }
 
 }
