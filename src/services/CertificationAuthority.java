63135eb26ac3e686c5239abaefdd0f2a7883bec6
triquell02 <poltri02@gmail.com>
2022-12-28 15:24:44 +0100

    Implementation of some methods in CertificationAuthority.java

--- src/services/CertificationAuthority.java
+++ src/services/CertificationAuthority.java
@@ -6,7 +6,9 @@ import services.exceptions.*;
 import java.util.Date;
 
 public interface CertificationAuthority {
+
     boolean sendPIN(Nif nif, Date date) throws NifNotRegisteredException, IncorrectValDateException, AnyMobileRegisteredException, ConnectException;
+
     boolean checkPIN(Nif nif, SmallCode pin) throws NotValidPINException, ConnectException;
 
     byte ckeckCredent (Nif nif, Password passw) throws NifNotRegisteredException, NotValidCredException, AnyMobileRegisteredException, ConnectException;
